3d5c0711e100b4797ea138ed06731cd87ec0d17f
Jorge Jesús <jorgektch@gmail.com>
2024-12-10 16:37:54 -0500

    Create E.cpp

---
+++ FISI Aniversario/E.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n); for(auto &x : a) cin >> x; sort(a.begin(), a.end());
+    if(k == 0) {
+        if(a[0] == 1)
+            cout << -1;
+        else
+            cout << 1;
+        return;
+    }
+    if(k == n){
+        cout << a.back();
+        return;
+    } else {
+        if(a[k] == a[k-1]) {
+            cout << -1;
+            return;
+        } else {
+            cout << a[k-1];
+            return;
+        }
+    }
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
